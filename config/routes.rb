9c3af2d2f464850b3192e249cd2a3ce4edab4856
Joshua Toyota <jearsh@gmail.com>
2024-01-26 19:49:47 -0500

    update gemfile and routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "monsters#index"
 end

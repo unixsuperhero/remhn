5925a46784455d8565a0971bb388f9a7146e0d15
Joshua Toyota <jearsh@gmail.com>
2024-01-31 01:38:14 -0500

    v2: calc_possible_weapons.rb -- extract NextGrade class

--- calc_possible_weapons.rb
+++ calc_possible_weapons.rb
@@ -1,7 +1,33 @@
 require 'csv'
+
 item_data = [294,179,76,8,255,274,75,69,34,5,273,257,102,67,30,3,263,295,57,47,31,6,216,219,70,48,20,3,72,235,3,50,12,2,299,97,36,30,13,6,248,245,57,53,21,nil,40,118,13,24,8,nil,194,14,2,32,8,2,88,11,16,22,21,2,88,11,4,11,5,1,40,51,9,7,7,nil,40,70,9,8,9,nil,6,2,nil,nil,2,nil,14,12,4,nil,1,nil,19,7,3,2,2,nil,188,161,25,30,18,nil,74,187,20,20,10,1,128,73,14,14,9,nil,2,2,nil,1,2,nil,nil,nil,nil,26,2,9,210,194,225,215,135,275,243,189,314,208,91,244,234,37,20,nil,131380]
 
 equip_data = [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,"4_4",nil,"5_4",nil,nil,nil,nil,nil,nil,"3_4",nil,nil,nil,nil,nil,nil,nil,nil,"2_3",nil,nil,"7_4",nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,"2_4","3_5","3_4",nil,nil,nil,nil,nil,nil,nil,nil,"4_4",nil,"4_4",nil,"2_4",nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,"5_2","5_4",nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,"6_2",nil,nil,nil,nil,nil,nil,nil,nil,nil,"5_2",nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,"5_2",nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,"4_4",nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,"3_4"]
+
+class NextGrade
+  attr_reader :current_grade, :lowest_grade
+  def initialize(current_grade, lowest_grade)
+    @current_grade = current_grade
+    @lowest_grade = lowest_grade
+  end
+
+  def next_grade
+    if current_grade.nil? || current_grade.strip.length == 0
+      return [lowest_grade.grade, lowest_grade.sub_grade]
+    end
+
+    x, y = current_grade.split('_').map(&:to_i)
+
+    if y == 5
+      x += 1
+      y = 1
+    else
+      y += 1
+    end
+
+    [x, y]
+  end
+end
 
 File.open('output.log', 'a') do |log|
   can_do = []
@@ -11,21 +37,7 @@ File.open('output.log', 'a') do |log|
     possible = false
     last = nil
     fst = weap.equip_grades.order(:grade, :sub_grade).first
-    grade = fst.grade
-    subgrade = fst.sub_grade
-    if equip_data[weap.id - 1]
-      log.puts(before: [grade, subgrade], current_lvl: equip_data[weap.id - 1])
-      ngr, nsgr = equip_data[weap.id - 1].split(?_)
-      if nsgr == '5'
-        grade = ngr.to_i + 1
-        subgrade = 1
-        log.puts(ngr: ngr, nsgr: nsgr, nsgr_is_5: [grade, subgrade])
-      else
-        grade = ngr.to_i
-        subgrade = nsgr.to_i + 1
-        log.puts(ngr: ngr, nsgr: nsgr, nsgr_is_not_5: [grade, subgrade])
-      end
-    end
+    grade, subgrade = NextGrade.new(equip_data[weap.id - 1], fst).next_grade
     weap.equip_grades.where("grade >= ? AND sub_grade >= ?", grade, subgrade).order(:grade, :sub_grade).each do |gr|
       egis = gr.equip_grade_items
       log.puts egis.map{|egi| [egi.item.name, egi.qty] }

c2f9b40e547ea302bfe265fe63d98c74d7396d74
Joshua Toyota <jearsh@gmail.com>
2024-01-30 23:27:13 -0500

    add sort_order column to Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_29_064259) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_31_042458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_29_064259) do
     t.integer "rarity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sort_order"
   end
 
   create_table "location_areas", force: :cascade do |t|
